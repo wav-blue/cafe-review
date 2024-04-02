83cf30fe503b37aa9dd3fffa62010286b14174a1
박수진 <wav0120@gmail.com>
2024-04-02 19:51:57 +0900

    Feat: 에러 핸들링 관련 코드, 템플릿 추가

---
+++ src/main/java/kr/sujin/cafereview/lib/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package kr.sujin.cafereview.lib;
+
+import java.util.NoSuchElementException;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handle(IllegalArgumentException exception) {
+        return ResponseEntity.badRequest().body("IllegalArgumentException 발생!");
+    }
+    
+    @ExceptionHandler(EntityNotFoundException.class)
+    public String handle(EntityNotFoundException exception) {
+        return "error/NotFound";
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<String> handle(IllegalStateException exception) {
+        return ResponseEntity.badRequest().body("이미 처리된 요청입니다!");
+    }
+}
