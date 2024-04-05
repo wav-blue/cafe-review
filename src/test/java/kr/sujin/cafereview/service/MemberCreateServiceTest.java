adeb665398352aeaa067f3560b33c3dabcd19f70
박수진 <wav0120@gmail.com>
2024-04-05 18:16:04 +0900

    Test: Member 생성 테스트 ADMIN 추가

--- src/test/java/kr/sujin/cafereview/service/MemberCreateServiceTest.java
+++ src/test/java/kr/sujin/cafereview/service/MemberCreateServiceTest.java
@@ -28,7 +28,7 @@ public class MemberCreateServiceTest {
     MemberRepository memberRepository;
 
     @Test
-    @DisplayName("리뷰 생성 테스트")
+    @DisplayName("회원(USER) 생성 테스트")
     @WithMockUser(username = "testEmail1@exam.com", roles = "ADMIN")
     void saveMember() throws Exception{
         MemberFormDto memberFormDto = new MemberFormDto();
@@ -42,6 +42,43 @@ public class MemberCreateServiceTest {
         memberFormDto.setPassword(testPassword);
         memberFormDto.setAddress("테스트 주소");
         memberFormDto.setIsAdmin(false);
+
+        Optional<Member> alreadyMember =  memberRepository.findByEmail(testEmail);
+        
+        if(alreadyMember.isPresent()){
+            fail("테스트하려는 이메일로 등록된 유저가 존재함");
+        }
+
+        Member member = memberCreateService.saveMember(memberFormDto);
+
+        Optional<Member> savedMember =  memberRepository.findByEmail(testEmail);
+
+        if(savedMember.isEmpty()){
+            fail("테스트로 저장된 내역 조회 실패");
+        } 
+
+        if (savedMember.isPresent()) {
+            assertEquals(Optional.of(member), savedMember);
+        }
+
+    }
+
+    
+    @Test
+    @DisplayName("회원(ADMIN) 생성 테스트")
+    @WithMockUser(username = "testEmail1@exam.com", roles = "ADMIN")
+    void saveMemberAdmin() throws Exception{
+        MemberFormDto memberFormDto = new MemberFormDto();
+
+        String testName = "테스트용 계정";
+        String testEmail = "testEmail9999@test.net";
+        String testPassword = "testEmail";
+
+        memberFormDto.setName(testName);
+        memberFormDto.setEmail(testEmail);
+        memberFormDto.setPassword(testPassword);
+        memberFormDto.setAddress("테스트 주소");
+        memberFormDto.setIsAdmin(true);
 
         Optional<Member> alreadyMember =  memberRepository.findByEmail(testEmail);
         
