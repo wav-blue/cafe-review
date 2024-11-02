a82577739b24c503591c3814ab4b6694481961d7
박수진 <wav0120@gmail.com>
2024-11-02 23:48:52 +0900

    Feat: Redis Config 변경

--- src/main/java/kr/sujin/cafereview/config/RedisConfig.java
+++ src/main/java/kr/sujin/cafereview/config/RedisConfig.java
@@ -1,5 +1,7 @@
 package kr.sujin.cafereview.config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
