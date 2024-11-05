06043430c039fbfa7df3d6e42fbf32ac02a7397e
박수진 <wav0120@gmail.com>
2024-11-05 10:04:48 +0900

    Feat: redis 데이터를 Map 형태로 읽는 메소드 추가

--- src/main/java/kr/sujin/cafereview/service/bookmark/RecentBookmarkReadService.java
+++ src/main/java/kr/sujin/cafereview/service/bookmark/RecentBookmarkReadService.java
@@ -1,7 +1,9 @@
 package kr.sujin.cafereview.service.bookmark;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
@@ -21,6 +23,22 @@ public class RecentBookmarkReadService {
         }
         for(String e: reviewIds.split(" ")){
             castReviewIds.add(Long.valueOf(e));
+        }
+        return castReviewIds;
+    }
+
+    public Map<Long, Integer> readRecentBookmarkReviewIdsWithMap(){
+        String reviewIds = redisRepository.getValues("RecentBookmarkReviews");
+        Map<Long, Integer> castReviewIds = new HashMap<>();
+
+        if(reviewIds.length()==0){
+            return castReviewIds;
+        }
+        for(String e: reviewIds.split(" ")){
+            if(castReviewIds.containsKey(Long.valueOf(e))){
+                castReviewIds.put(Long.valueOf(e), castReviewIds.get(e)+1);
+            }
+            castReviewIds.put(Long.valueOf(e), 1);
         }
         return castReviewIds;
     }
